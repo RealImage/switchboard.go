53ad511e36bc13c716083cb6d468304ca1c17e9d
Sudhir Jonathan <sudhir.j@gmail.com>
2018-08-28 15:09:15 +0530

    Reverted mistake

--- board_test.go
+++ board_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 func TestBoardCreation(t *testing.T) {
-	supplies := []switchboard.Supply{{factor: 1}, {factor: 1.1}}
-	demands := []switchboard.Demand{{cost: 1}, {cost: 2}}
+	supplies := []switchboard.Supply{testSupply{factor: 1}, testSupply{factor: 1.1}}
+	demands := []switchboard.Demand{testDemand{cost: 1}, testDemand{cost: 2}}
 	board := switchboard.NewBoard(supplies, demands)
 	assert.Equal(t, supplies, board.Supplies())
 	assert.Equal(t, demands, board.Demands())
